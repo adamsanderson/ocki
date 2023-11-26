956d83da781e107425da07414e38755d95b7bb58
Adam Sanderson <adams@textio.com>
2023-11-26 09:16:54 -0800

    Implements game mechanics

--- src/game/Board.ts
+++ src/game/Board.ts
@@ -1,7 +1,8 @@
 import { Container } from "pixi.js";
 import { Piece } from "./Piece";
+import anime from "animejs";
 
-type MaybePiece = Piece|null;
+type MaybePiece = Piece | null;
 type GameColumn = MaybePiece[];
 
 export class GameBoard extends Container {
@@ -9,28 +10,152 @@ export class GameBoard extends Container {
   private scaleX = 64 + 2;
   private scaleY = 64 + 10;
 
+  private hoveredPieces: Set<Piece>;
+
   constructor(
-    private rowCount = 8, 
+    private rowCount = 8,
     private colCount = 8,
   ) {
     super()
     this.board = new Array(colCount);
+    this.hoveredPieces = new Set();
 
-    for (let c = 0; c < colCount; c++) {
+    for (let i = 0; i < colCount; i++) {
       const col = new Array(rowCount) as GameColumn
-      for (let r = 0; r < rowCount; r++) {
-        const piece = Piece.random(c * this.scaleX, r * this.scaleY);
+      for (let j = 0; j < rowCount; j++) {
+        const piece = Piece.random(i * this.scaleX, j * this.scaleY);
+        piece.i = i;
+        piece.j = j;
         this.addChild(piece);
         this.listenToPiece(piece);
-        col[r] = piece;
+        col[j] = piece;
       }
-      this.board[c] = col;
+      this.board[i] = col;
     }
   }
 
   listenToPiece(piece: Piece) {
-    piece.on('pointerover', () => {
-      console.log(piece.type)
-    });
+    piece.on('pointerover', () => this.hoverPiece(piece));
+    piece.on('pointerup', () => this.clickPiece(piece));
+  }
+
+  hoverPiece(piece: Piece) {
+    if (!this.hoveredPieces.has(piece)) {
+      this.hoveredPieces.forEach(p => p.setIsHovered(false));
+      this.hoveredPieces = this.getConnected(piece);
+      if (this.hoveredPieces.size > 1) {
+        this.hoveredPieces.forEach(p => p.setIsHovered(true));
+      }
+    }
+  }
+
+  async clickPiece(piece: Piece) {
+    this.hoverPiece(piece);
+    if (this.hoveredPieces.size > 1) {
+      this.hoveredPieces.forEach(p => this.removePiece(p));
+      this.removeChild(...this.hoveredPieces);
+
+      this.eventMode = 'none';
+      await this.dropPieces();
+      await this.shiftPieces();
+      this.eventMode = 'auto';
+    }
+  }
+
+  getConnected(piece: Piece) {
+    const connected = new Set<Piece>();
+
+    const candidates = [piece];
+    let nextPiece: MaybePiece;
+
+    while (candidates.length > 0) {
+      const current = candidates.pop() as Piece;
+      connected.add(current);
+
+      nextPiece = this.getPiece(current.i - 1, current.j);
+      if (nextPiece?.type === piece.type && !connected.has(nextPiece)) candidates.push(nextPiece!);
+
+      nextPiece = this.getPiece(current.i + 1, current.j);
+      if (nextPiece?.type === piece.type && !connected.has(nextPiece)) candidates.push(nextPiece!);
+
+      nextPiece = this.getPiece(current.i, current.j - 1);
+      if (nextPiece?.type === piece.type && !connected.has(nextPiece)) candidates.push(nextPiece!);
+
+      nextPiece = this.getPiece(current.i, current.j + 1);
+      if (nextPiece?.type === piece.type && !connected.has(nextPiece)) candidates.push(nextPiece!);
+    }
+
+    return connected;
+  }
+
+  getPiece(i: number, j: number): Piece | null {
+    if (i >= 0 && i < this.colCount && j >= 0 && j < this.rowCount) {
+      return this.board[i][j];
+    }
+
+    return null;
+  }
+
+  removePiece(piece: Piece) {
+    const toRemove = this.board[piece.i][piece.j];
+    if (piece !== toRemove) throw new Error(`Piece mismatch at ${piece.i},${piece.j}`)
+    this.board[piece.i][piece.j] = null;
+  }
+
+  dropPieces() {
+    const promises: Promise<void>[] = [];
+    for (let i = 0; i < this.colCount; i++) {
+      const col = this.board[i];
+      const filtered = col.filter(p => p)
+      const newCol = new Array<MaybePiece>(this.rowCount - filtered.length).fill(null).concat(filtered);
+
+      // Update the `j` position for each piece
+      for (let j = 0; j < this.rowCount; j++) {
+        const piece = newCol[j];
+        if (piece && piece.j !== j) {
+          promises.push(
+            anime({
+              targets: piece,
+              y: j * this.scaleY,
+              easing: 'easeOutBounce',
+              duration: (j - piece.j) * 250
+            }).finished
+          );
+
+          piece.j = j;
+        }
+      }
+
+      this.board[i] = newCol;
+    }
+    return Promise.all(promises);
+  }
+
+  shiftPieces() {
+    const promises: Promise<void>[] = [];
+    const filtered = this.board.filter(col => col.some(p => p));
+    const emptyCol = new Array(this.rowCount).fill(null);
+    const newBoard = new Array<MaybePiece[]>(this.colCount - filtered.length).fill(emptyCol).concat(filtered);
+
+    for (let i = 0; i < this.colCount; i++) {
+      for (let j = 0; j < this.rowCount; j++) {
+        const piece = newBoard[i][j];
+        if (piece && piece.i !== i) {
+          promises.push(
+            anime({
+              targets: piece,
+              x: i * this.scaleX,
+              easing: 'easeInOutQuad',
+              duration: (i - piece.i) * 50
+            }).finished
+          );
+          
+          piece.i = i;
+        }
+      }
+    }
+
+    this.board = newBoard;
+    return Promise.all(promises);
   }
 }
--- src/game/Piece.ts
+++ src/game/Piece.ts
@@ -1,20 +1,35 @@
-import { Container, FederatedEventHandler, FederatedPointerEvent, Sprite } from "pixi.js"
+import { ColorMatrixFilter, Container, Sprite, Filter } from "pixi.js"
 
 const PIECE_TYPES = ['tile-1', 'tile-2', 'tile-3', 'tile-4', 'tile-5'] as const
 type PieceType = typeof PIECE_TYPES[number]
 
+const HOVER_FILTER = new ColorMatrixFilter();
+HOVER_FILTER.brightness(1.25, false);
+HOVER_FILTER.contrast(0.6, false);
+
+const DEFAULT_FILTER_SET: Filter[] = [];
+const HOVER_FILTER_SET: Filter[] = [HOVER_FILTER];
+
 export class Piece extends Container {
   private sprite: Sprite;
+  private isHovered: boolean;
   public readonly type: PieceType;
+  private _i: number;
+  private _j: number;
 
   constructor(type: PieceType, x: number, y: number) {
     super()
     this.type = type;
+    this._i = 0;
+    this._j = 0;
+
     this.sprite = Sprite.from(type)
-    this.sprite.x = x;
-    this.sprite.y = y;
+    this.x = x;
+    this.y = y;
+    
     this.addChild(this.sprite);
-    this.eventMode = 'static'
+    this.eventMode = 'static';
+    this.isHovered = false;
   }
   
   static random(x: number, y: number): Piece {
@@ -24,5 +39,30 @@ export class Piece extends Container {
   
   is(type: PieceType) {
     return this.type === type;
+  }
+
+  set i(value: number) {
+    this._i = value;
+  }
+
+  get i() {
+    return this._i;
+  }
+
+  set j(value: number) {
+    this._j = value;
+  }
+
+  get j() {
+    return this._j;
+  }
+
+  setIsHovered(hovered: boolean) {
+    this.isHovered = hovered;
+    if (this.isHovered) {
+      this.filters = HOVER_FILTER_SET;
+    } else {
+      this.filters = DEFAULT_FILTER_SET;
+    }
   }
 }
