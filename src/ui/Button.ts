9f0c89c9941e2757bff8ac026cbc3544926c34f1
Adam Sanderson <netghost@gmail.com>
2023-12-16 22:30:17 -0800

    Remove commented out code

--- src/ui/Button.ts
+++ src/ui/Button.ts
@@ -55,36 +55,5 @@ export class Button extends FancyButton {
         y: -2
       }
     }
-
-    // this.onDown.connect(this.handleDown.bind(this));
-    // this.onUp.connect(this.handleUp.bind(this));
-    // this.onHover.connect(this.handleHover.bind(this));
-    // this.on('pointerupoutside', this.handleUp.bind(this));
-    // this.on('pointerout', this.handleUp.bind(this));
   }
-
-  // /** Show the component */
-  // public async show(animated = true) {
-  //   gsap.killTweensOf(this.pivot);
-  //   this.visible = true;
-  //   if (animated) {
-  //     this.pivot.y = -200;
-  //     await gsap.to(this.pivot, { y: 0, duration: 0.5, ease: 'back.out' });
-  //   } else {
-  //     this.pivot.y = 0;
-  //   }
-  //   this.interactiveChildren = true;
-  // }
-
-  // /** Hide the component */
-  // public async hide(animated = true) {
-  //   this.interactiveChildren = false;
-  //   gsap.killTweensOf(this.pivot);
-  //   if (animated) {
-  //     await gsap.to(this.pivot, { y: -200, duration: 0.3, ease: 'back.in' });
-  //   } else {
-  //     this.pivot.y = -200;
-  //   }
-  //   this.visible = false;
-  // }
 }
