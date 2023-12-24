d35a0c9a6e22624aa666c943b9ffbad821c1dc64
Adam Sanderson <netghost@gmail.com>
2023-12-23 23:01:45 -0800

    Basic vite build config

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: '/ocki/',
+  build: {
+    rollupOptions: {
+      // https://rollupjs.org/configuration-options/
+    },
+  },
+})
