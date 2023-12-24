46c1b1b82059488b429215b7bd44c561c6310eaf
Adam Sanderson <netghost@gmail.com>
2023-12-23 22:52:33 -0800

    Fixi minor linting

--- src/screens/LoadScreen/Logo.ts
+++ src/screens/LoadScreen/Logo.ts
@@ -1,19 +1,17 @@
 import { Container, Sprite } from 'pixi.js';
-import anime, { AnimeInstance } from 'animejs';
+import anime from 'animejs';
 
 /** Placeholder logo UI component */
 export class Logo extends Container {
     /** The logo image */
     private image: Sprite;
 
-    private animation: AnimeInstance;
-
     constructor() {
         super();
         this.image = Sprite.from('logo');
         this.image.anchor.set(0.5);
         this.addChild(this.image);
-        this.animation = anime({
+        anime({
           targets: this.scale,
           x: [0.95,1],
           y: [0.95,1],
